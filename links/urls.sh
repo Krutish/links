941818cac990b13bbc1233cff95664cd36f5fd68
root <root@debian-s-6vcpu-16gb-fra1-01>
2020-05-30 13:33:48 +0000

    Second object

---
+++ links/urls.sh
@@ -0,0 +1,21 @@
+OpenVPN:
+git.io/vpn
+
+Asterisk:
+git.io/asterisk.sh
+
+Docker:
+get.docker.com
+
+Nginx:
+git.io/nginx
+
+Apache2:
+github.com/apache/httpd
+
+Curl:
+github.com/curl/curl
+git.io/curl
+
+VCPKG ( ,   curl  ..):
+github.com/Microsoft/vcpkg/
